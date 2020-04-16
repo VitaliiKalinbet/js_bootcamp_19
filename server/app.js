49556339c674ff045d81f19b38195ffacba98941
VitaliiKalinbet <bosssvit@gmail.com>
2020-04-16 13:38:15 +0300

    done websocket module 14

--- server/app.js
+++ server/app.js
@@ -1,25 +1,38 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 const history = [];
 
-io.on('connection', socket => {
-  console.log('User connected');
+io.on("connection", (socket) => {
+  console.log("User connected");
 
-  socket.emit('connected', history);
+  socket.emit("connected", history);
 
-  socket.on('chat-message', message => {
+  socket.on("chat-message", (message) => {
     history.push(message);
-    socket.broadcast.emit('chat-message', message);
+
+    socket.broadcast.emit("chat-message", message);
+
     console.log(`Received message - ${JSON.stringify(message)}`);
   });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on port 3000');
+http.listen(5000, () => {
+  console.log("listening on port 5000");
 });
+
+// варианты подписки на событие:
+// xyz.addListener;
+// xyz.on;
+// xyz.listen;
+// xyz.scan;
+
+// варианты создания события:
+// xyz.emit;
+// xyz.send;
+// xyz.fire;
